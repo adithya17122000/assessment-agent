35cb1576854d7df870ced39ac9543ad4b2133f2f
gururaja-br <gururaja.bhat@triconinfotech.com>
2026-07-09 14:31:12 +0530

    added orchestration(take assessment) endpoint

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.api.eligibility_routes import router as eligibility_router
 from app.api.assessment_routes import router as assessment_router
 from app.api.question_generation_routes import router as question_generation_router
+from app.api.orchestration_routes import router as orchestration_router
 
 
 app = FastAPI(title="Team 4 - Assessment Service")
@@ -9,3 +10,4 @@ app = FastAPI(title="Team 4 - Assessment Service")
 app.include_router(eligibility_router)
 app.include_router(assessment_router)
 app.include_router(question_generation_router)
+app.include_router(orchestration_router)
